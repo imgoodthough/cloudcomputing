550bd6362d7b257d5af40f54aa35f3efd5ed189d
20165122 <rlagnlrhs2k@naver.com>
2021-11-22 17:57:57 +0900

    2021.11.22 05:56 st279,dt73 1min_update

--- src/bikepublisher/DBConnect.java
+++ src/bikepublisher/DBConnect.java
@@ -12,50 +12,64 @@ public class DBConnect {
 	private static final String USERNAME = "dba"; // DB ID static final 
 	private static String PASSWORD = "hello"; // DB Password
 	static Statement stmt = null;
+	String sql = "";
 	
 
 
 	public void DBConnct(String createdate, String stationname, int parkingbiketotcnt, String stationid){
-		System.out.print("User Table 접속 : "); 
-		String sql = "INSERT INTO st4(createdate, stationname, parkingbiketotcnt, stationid)" + "VALUES (?, ?, ?, ?)";
+			
+			if(stationid.equals("ST-73")) {
+				System.out.println(stationid);
+				sql = "INSERT INTO st4(createdate, stationname, parkingbiketotcnt, stationid)" + "VALUES (?, ?, ?, ?)";
+			}else if(stationid.equals("ST-279")) {
+				System.out.println(stationid);
+				sql = "INSERT INTO st279(createdate, stationname, parkingbiketotcnt, stationid)" + "VALUES (?, ?, ?, ?)";
+			}
+			
+			System.out.println(sql);
+			
 		
-		try {
-			System.out.println("생성자");
-			Class.forName("cubrid.jdbc.driver.CUBRIDDriver");
-			conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-			System.out.println("드라이버 로딩 성공");
+			System.out.print("User Table 접속 : "); 
 			
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-
-			pstmt.setString(1, createdate);
-			pstmt.setString(2, stationname);
-			pstmt.setInt(3, parkingbiketotcnt);
-			pstmt.setString(4, stationid);
-			pstmt.executeUpdate();
-			
-			pstmt.close();
-	
-			conn.close();
-			
-			
-			/*데이터 출력하고 싶을 때
-			 * ResultSet rs = stmt.executeQuery("SELECT * FROM st4");
-			 * while(rs.next()) {
-				String qfw = rs.getString("createdate");
-				
-				System.out.println(qfw);
-			}*/
-			
-		} catch (Exception e) {
-			System.out.println("드라이버 로딩 실패" + e.getMessage());
 			try {
+				System.out.println("생성자");
+				Class.forName("cubrid.jdbc.driver.CUBRIDDriver");
+				conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+				System.out.println("드라이버 로딩 성공");
+				
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+
+				pstmt.setString(1, createdate);
+				pstmt.setString(2, stationname);
+				pstmt.setInt(3, parkingbiketotcnt);
+				pstmt.setString(4, stationid);
+				pstmt.executeUpdate();
+				
+				pstmt.close();
+		
 				conn.close();
-			} catch(SQLException e1) { }
+				
+				
+				/*데이터 출력하고 싶을 때
+				 * ResultSet rs = stmt.executeQuery("SELECT * FROM st4");
+				 * while(rs.next()) {
+					String qfw = rs.getString("createdate");
+					
+					System.out.println(qfw);
+				}*/
+				
+			} catch (Exception e) {
+				System.out.println("드라이버 로딩 실패" + e.getMessage());
+				try {
+					conn.close();
+				} catch(SQLException e1) { }
+			}
+			
+			
+			
+		
 		}
 		
-		
-		
-	}
 		
 			
 
--- src/bikepublisher/bikepublisher.java
+++ src/bikepublisher/bikepublisher.java
@@ -34,6 +34,11 @@ public class bikepublisher {
 		String stationName = "";//거치소 이름 저장할 변수
 		int parkingBikeTotCnt; // 자전거 주차 총 건수
 		String stationId = ""; //대여소 ID
+		
+		String result2 = "";//json저장할 string 변수
+		String stationName2 = "";//거치소 이름 저장할 변수
+		int parkingBikeTotCnt2; // 자전거 주차 총 건수
+		String stationId2 = ""; //대여소 ID
 		while(true) {
 			try {
 		//저장 현재 시간 만들기
@@ -41,13 +46,18 @@ public class bikepublisher {
 				SimpleDateFormat time= new SimpleDateFormat("yyyy/MM/dd HH:mm");
 				String recordtime = time.format(date); // 현재 시각 저장
 				DBConnect db = new DBConnect();
+				DBConnect db2 = new DBConnect();
 				
 		//실시간 api에서 데이터 가져와 파싱 후 출력
 		
 			
-				URL url = new URL("http://openapi.seoul.go.kr:8088/6649487853726c613131336750435046/json/bikeList/90/90/");
+				URL url = new URL("http://openapi.seoul.go.kr:8088/6649487853726c613131336750435046/json/bikeList/90/90/"); //여의나루역 1번출구 앞 따릉이 대여소
+				URL url2 = new URL("http://openapi.seoul.go.kr:8088/6649487853726c613131336750435046/json/bikeList/109/109/"); //양평1 보행육교 따릉이 대여소
 				BufferedReader bf;
+				BufferedReader bf2;
+				
 				bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+				bf2 = new BufferedReader(new InputStreamReader(url2.openStream(), "UTF-8"));
 				
 				result = bf.readLine();
 				JSONParser jsonParser = new JSONParser();
@@ -60,8 +70,21 @@ public class bikepublisher {
 				parkingBikeTotCnt = Integer.parseInt(rowinfo.get("parkingBikeTotCnt").toString()); // 자전거 주차 총 건수 int로 저장
 				stationId = rowinfo.get("stationId").toString(); // 대여소 ID 저장
 				
-				db.DBConnct(recordtime, stationName, parkingBikeTotCnt, stationId);
 				
+				result2 = bf2.readLine();
+				JSONParser jsonParser2 = new JSONParser();
+				JSONObject jsonObject2 = (JSONObject)jsonParser2.parse(result2);
+				JSONObject rentBikeStatus2 = (JSONObject)jsonObject2.get("rentBikeStatus");
+				JSONArray row2 = (JSONArray)rentBikeStatus2.get("row");
+				JSONObject rowinfo2 = (JSONObject)row2.get(0); 
+				
+				stationName2 = rowinfo2.get("stationName").toString(); // 대여소 이름 저장
+				parkingBikeTotCnt2 = Integer.parseInt(rowinfo2.get("parkingBikeTotCnt").toString()); // 자전거 주차 총 건수 int로 저장
+				stationId2 = rowinfo2.get("stationId").toString(); // 대여소 ID 저장
+				System.out.println(stationName2 + stationName);
+				
+				db.DBConnct(recordtime, stationName, parkingBikeTotCnt, stationId);
+				db2.DBConnct(recordtime, stationName2, parkingBikeTotCnt2, stationId2);
 				Thread.sleep(60000);
 				//Thread.sleep(1800000);//30분에 한번씩 추출
 				
